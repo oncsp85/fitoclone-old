cd6f922aa2536f175109a4506f8d50871cf51125
Shaun Perkins <shaun.a.perkins85@gmail.com>
2020-02-27 11:27:29 +0000

    separated the renderWorkout method into its own Component for potential re-use later

---
+++ src/Workout/Workout.js
@@ -0,0 +1,136 @@
+import React from 'react'
+import './Workout.css';
+
+const Workout = ({workout}) => {
+  return (
+    <div className="workout" key={workout.date + workout.workout_id}>
+      <h2 className="date">
+        {formatDate(workout.date)}
+        {
+          workout.exercises
+            .sort((a, b) => a.exercise_id - b.exercise_id)
+            .map(exercise => renderExercise(exercise))
+        }
+      </h2>
+    </div>
+  );
+}
+
+  const formatDate = (dateString) => {
+    return new Date(dateString).toLocaleDateString("en-GB", {
+      weekday: "long", day: "numeric", month: "short", year:"numeric"
+    });
+  }
+
+  const formatTime = (seconds) => {
+    const time = new Date(0,0,0,0,0, seconds);
+    return time.toLocaleTimeString("en-GB", {
+      hour12: false
+    });
+  }
+
+  const renderCardioSet = (set) => {
+    const { time, distance, speed, avhr, weight, resistance, other } = set;
+    // Initial property; cardio exercises always have a time.
+    const properties = [
+      <span key="time">
+        {formatTime(time)}
+      </span>
+    ];
+    // All of the following properties are optional
+    if (distance)
+      properties.push(
+        <span key="distance">
+          {" | "}
+          {distance.value} {distance.unit}
+        </span>
+      );
+      if (set.speed)
+      properties.push(
+        <span key="speed">
+          {" | "}
+          {speed.value} {speed.unit}
+        </span>
+      );
+      if (avhr)
+      properties.push(
+        <span key="avhr">
+          {" | "}
+          {avhr} {"BPM"}
+        </span>
+      );
+      if (weight)
+      properties.push(
+        <span key="weight">
+          {" | "}
+          {weight.value} {weight.unit}
+        </span>
+      );
+      if (set.resistance)
+      properties.push(
+        <span key="resistance">
+          {" | "}
+          {resistance.value} {resistance.unit}
+        </span>
+      );
+      if (other)
+      properties.push(
+        <span key="other">
+          {" | "}
+          {other}
+        </span>
+      );
+
+    return (
+      <li key={set.set_id}>
+        {properties}
+      </li>
+    );
+  }
+
+  const renderWeightsSet = (set) => {
+    return (
+      <li key={set.set_id}>
+        <span>
+          {set.weight.value} {set.weight.unit}
+        </span>
+        {" x "}
+        <span>
+          {set.reps}
+          {" reps"}
+        </span>
+      </li>
+    );
+  }
+
+  const renderBodyweightSet = (set) => {
+    return (
+      <li key={set.set_id}>
+        <span>
+          {set.reps}
+          {" reps"}
+        </span>
+      </li>
+    );
+  }
+
+  const renderExercise = (exercise) => {
+    const setList = exercise.sets
+      .sort((a, b) => a.set_id - b.set_id)
+      .map(set => {
+      if (exercise.type === "weights")
+        return renderWeightsSet(set);
+      else if (exercise.type === "bodyweight")
+        return renderBodyweightSet(set);
+      else
+        return renderCardioSet(set);
+    });
+    return (
+      <div className="exercise" key={exercise.exercise_id}>
+        <h3 className="exercise-name">{exercise.name}</h3>
+        {setList}
+      </div>
+    );
+  }
+
+export default Workout;
--- src/WorkoutList/WorkoutList.js
+++ src/WorkoutList/WorkoutList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './WorkoutList.css';
+import Workout from "../Workout/Workout"
 
 class WorkoutList extends React.Component {
   constructor() {
@@ -19,140 +20,16 @@ class WorkoutList extends React.Component {
       });
   }
 
-  formatDate(dateString) {
-    return new Date(dateString).toLocaleDateString("en-GB", {
-      weekday: "long", day: "numeric", month: "short", year:"numeric"
-    });
-  }
-
-  formatTime(seconds) {
-    const time = new Date(0,0,0,0,0, seconds);
-    return time.toLocaleTimeString("en-GB", {
-      hour12: false
-    });
-  }
-
-  renderCardioSet(set) {
-    const { time, distance, speed, avhr, weight, resistance, other } = set;
-    const properties = [
-      <span key="time">
-        {this.formatTime(time)}
-      </span>
-    ];
-    if (distance)
-      properties.push(
-        <span key="distance">
-          {" | "}
-          {distance.value} {distance.unit}
-        </span>
-      );
-      if (set.speed)
-      properties.push(
-        <span key="speed">
-          {" | "}
-          {speed.value} {speed.unit}
-        </span>
-      );
-      if (avhr)
-      properties.push(
-        <span key="avhr">
-          {" | "}
-          {avhr} {"BPM"}
-        </span>
-      );
-      if (weight)
-      properties.push(
-        <span key="weight">
-          {" | "}
-          {weight.value} {weight.unit}
-        </span>
-      );
-      if (set.resistance)
-      properties.push(
-        <span key="resistance">
-          {" | "}
-          {resistance.value} {resistance.unit}
-        </span>
-      );
-      if (other)
-      properties.push(
-        <span key="other">
-          {" | "}
-          {other}
-        </span>
-      );
-
-    return (
-      <li key={set.set_id}>
-        {properties}
-      </li>
-    );
-  }
-
-  renderWeightsSet(set) {
-    return (
-      <li key={set.set_id}>
-        <span>
-          {set.weight.value} {set.weight.unit}
-        </span>
-        {" x "}
-        <span>
-          {set.reps}
-          {" reps"}
-        </span>
-      </li>
-    );
-  }
-
-  renderBodyweightSet(set) {
-    return (
-      <li key={set.set_id}>
-        <span>
-          {set.reps}
-          {" reps"}
-        </span>
-      </li>
-    );
-  }
-
-  renderExercise(exercise) {
-    const setList = exercise.sets
-      .sort((a, b) => a.set_id - b.set_id)
-      .map(set => {
-      if (exercise.type === "weights")
-        return this.renderWeightsSet(set);
-      else if (exercise.type === "bodyweight")
-        return this.renderBodyweightSet(set);
-      else
-        return this.renderCardioSet(set);
-    });
-    return (
-      <div className="exercise" key={exercise.exercise_id}>
-        <h3 className="exercise-name">{exercise.name}</h3>
-        {setList}
-      </div>
-    );
-  }
-
-  renderWorkout(workout) {
-    return (
-      <div className="workout" key={workout.date + workout.workout_id}>
-        <h2 className="date">
-          {this.formatDate(workout.date)}
-          {
-            workout.exercises
-              .sort((a, b) => a.exercise_id - b.exercise_id)
-              .map(exercise => this.renderExercise(exercise))
-          }
-        </h2>
-      </div>
-    );
-  }
-
   render() {
     return(
       <div className="workout-list">
-        {this.state.workouts.map(workout => this.renderWorkout(workout))}
+        {
+          this.state.workouts.map(workout => {
+            return (
+              <Workout workout={workout} />
+            );
+          })
+        }
       </div>
     );
   }
