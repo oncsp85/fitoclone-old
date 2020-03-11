f42224add60aba179355643ea2069a4dc32266d1
Shaun Perkins <shaun.a.perkins85@gmail.com>
2020-03-11 19:05:26 +0000

    changed Workout to a functional component with hooks

--- src/Form/Workout.js
+++ src/Form/Workout.js
@@ -1,72 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Exercise from './Exercise';
 
-class Workout extends React.Component {
-  constructor(props) {
-    super(props);
+const Workout = () => {
 
-    this.today = new Date().toISOString().substring(0, 10);
-    // set the min value for the date picker to be 1 month ago
-    const minDate = new Date();
-    minDate.setMonth(minDate.getMonth() - 1);
-    this.minDate = minDate.toISOString().substring(0, 10);
+  const today = new Date().toISOString().substring(0, 10);
+  // set the min value for the date picker to be 1 month ago
+  let minDate = new Date();
+  minDate.setMonth(minDate.getMonth() - 1);
+  minDate = minDate.toISOString().substring(0, 10);
 
-    this.state = { exercises: [], date: this.today };
+  const [ exercises, updateExercises ] = useState([]);
+  const [ date, changeDate ] = useState(today);
+
+  const addNewExercise = () => {
+    updateExercises([
+      ...exercises, {
+        exercise_id: exercises.length + 1,
+        name: "",
+        type: "weights",
+        sets: []
+      }
+    ]);
   }
 
-  addNewExercise = () => {
-    const exercises = this.state.exercises;
-    this.setState({
-      exercises: [
-        ...exercises, {
-          exercise_id: exercises.length + 1,
-          name: "",
-          type: "weights",
-          sets: []
-        }
-      ]
-    });
+  const updateSingleExercise = (newExercise) => {
+    const exercisesCopy = [...exercises];
+    exercisesCopy[newExercise.exercise_id - 1] = newExercise;
+    updateExercises(exercisesCopy);
   }
 
-  updateSingleExercise = (newExercise) => {
-    const exercises = this.state.exercises;
-    const updatedExercises = [...exercises];
-    updatedExercises[newExercise.exercise_id - 1] = newExercise;
-    this.setState({exercises: updatedExercises});
-  }
-
-  render() {
-    const { date, minDate, today, exercises } = this.state;
-    return(
-      <form>
-        <label>Workout Date</label>
-        <input type="date" 
-          value={ date }
-          min = { minDate }
-          max={ today }
-          onChange={ (e) => this.setState({date: e.target.value}) }
-        />
-        <button type="button" onClick={ this.addNewExercise } >
-          Add New Exercise
-        </button>
-        { 
-          exercises.map(exercise => {
-            return (
-              <Exercise 
-                key={ exercise.exercise_id } 
-                exercise={ exercise }
-                updateWorkout={ this.updateSingleExercise }
-              />
-            );
-          }) 
-        }
-        <br />
-        <button type="button"
-          onClick={() => console.log(exercises)}
-        >Submit Workout</button>
-      </form>
-    );
-  }
+  return(
+    <form>
+      <label>Workout Date</label>
+      <input type="date" 
+        value={ date }
+        min = { minDate }
+        max={ today }
+        onChange={ (e) => changeDate(e.target.value) }
+      />
+      <button type="button" onClick={ addNewExercise } >
+        Add New Exercise
+      </button>
+      { 
+        exercises.map(exercise => {
+          return (
+            <Exercise 
+              key={ exercise.exercise_id } 
+              exercise={ exercise }
+              updateWorkout={ updateSingleExercise }
+            />
+          );
+        }) 
+      }
+      <br />
+      <button type="button"
+        onClick={() => console.log(exercises)}
+      >Submit Workout</button>
+    </form>
+  );
 }
 
 export default Workout;
