0ff19cdaf26e86b1731b2356653e72a17471ac1a
Shaun Perkins <shaun.a.perkins85@gmail.com>
2020-04-21 11:38:18 +0100

    changed WorkoutForm state to store a workout object rather than an array of exercises. doesn't change anything at the moment but it will eventually mean I can pass an existing workout to it and have that load up within the form, which will allow me to reuse the form for editing a workout

--- src/Form/Workout.js
+++ src/Form/Workout.js
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
 import Exercise from './Exercise';
 
-const Workout = ({ exercises, updateSingleExercise, deleteExercise, deleteExerciseSet, changeRoute }) => {
+const Workout = ({ workout, updateSingleExercise, deleteExercise, 
+    deleteExerciseSet, changeRoute }) => {
 
   const today = new Date().toISOString().substring(0, 10);
   // set the min value for the date picker to be 1 month ago
   let minDate = new Date();
   minDate.setMonth(minDate.getMonth() - 1);
   minDate = minDate.toISOString().substring(0, 10);
-  const [ date, changeDate ] = useState(today);
+  const [ date, changeDate ] = useState(
+    workout.date ? workout.date : today
+  );
 
   const submitWorkout = async () => {
     // Count the number of workouts on the same day to get the workoutID
@@ -19,13 +22,11 @@ const Workout = ({ exercises, updateSingleExercise, deleteExercise, deleteExerci
     `&year=${workoutDate.getFullYear()}`;
     const numberOfWorkouts = await fetch(url)
       .then(response => response.json())
-      .then(workouts => {
-        return workouts.length
-      });
+      .then(workouts => workouts.length);
 
-    const exercisesCopy = [ ...exercises ];
+    const exercises = [ ...workout.exercises ];
     
-    exercisesCopy.forEach(exercise => {
+    exercises.forEach(exercise => {
       // If exercise has hours/mins/secs, convert to a single time property
       if ("hours" in exercise.sets[0]) {
         exercise.sets.forEach(set => {
@@ -44,29 +45,55 @@ const Workout = ({ exercises, updateSingleExercise, deleteExercise, deleteExerci
     });
 
     // Make a new workout object from the exercises array in state
-    const workout = {
-      workout_id: numberOfWorkouts + 1,
-      date: date,
-      exercises: exercisesCopy
+    const output = {
+      ...workout,
+      ...{
+        workout_id: numberOfWorkouts + 1,
+        date: date,
+        exercises: exercises
+      }
     };
 
     // Pass workout to API, wait for a response, then redirect to workout view
     await fetch("http://localhost:3001/workouts", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(workout)
+      body: JSON.stringify(output)
     }).then(() => changeRoute("show"));
   }
 
   // FOR TESTING WITHOUT CONNECTING TO DB, REMOVE LATER
   const submitWorkout2 = () => {
-    const workout = {
-      workout_id: 1,
-      date: date,
-      exercises: exercises
+    const exercises = [ ...workout.exercises ];
+    
+    exercises.forEach(exercise => {
+      // If exercise has hours/mins/secs, convert to a single time property
+      if ("hours" in exercise.sets[0]) {
+        exercise.sets.forEach(set => {
+          const time = Number(set["hours"]) * 3600 
+            + Number(set["mins"]) * 60 + Number(set["secs"]);
+          set["time"] = time;
+          delete set.hours;
+          delete set.mins;
+          delete set.secs;
+        });
+      }
+    
+      // Remove the field lists from the exercise object
+      delete exercise.required;
+      delete exercise.optional;
+    });
+    
+    const output = {
+      ...workout,
+      ...{
+        workout_id: 1,
+        date: date,
+        exercises: workout.exercises
+      }
     };
 
-    console.log(workout);
+    console.log(output);
   }
 
   return(
@@ -81,7 +108,7 @@ const Workout = ({ exercises, updateSingleExercise, deleteExercise, deleteExerci
           onChange={ (e) => changeDate(e.target.value) }
         />
         { 
-          exercises.map(exercise => {
+          workout.exercises.map(exercise => {
             return (
               <Exercise 
                 key={ exercise.exercise_id } 
@@ -95,7 +122,7 @@ const Workout = ({ exercises, updateSingleExercise, deleteExercise, deleteExerci
         }
         <br />
         <button type="button"
-          onClick={ submitWorkout }  // CHANGE BACK TO submitWorkout WHEN DONE TESTING
+          onClick={ submitWorkout2 }  // CHANGE BACK TO submitWorkout WHEN DONE TESTING
         >Submit Workout</button>
       </form>
     </div>
--- src/Form/WorkoutForm.js
+++ src/Form/WorkoutForm.js
@@ -3,15 +3,19 @@ import ExercisePicker from './ExercisePicker';
 import Workout from './Workout';
 import './WorkoutForm.css';
 
-const WorkoutForm = ({ changeRoute }) => {
+const WorkoutForm = (props) => {
 
-  const [ exercises, updateExercises ] = useState([]);
+  // If a workout has been passed in, use that workout, else make a new one
+  const [ workout, updateWorkout ] = useState(
+    props.workout ? props.workout : { exercises: [] }
+  );
 
   // Add a new blank exercise
   const addExercise = (exercise) => {
-    updateExercises([
-      ...exercises, {
-        exercise_id: exercises.length + 1,
+    // Add exercise to exercises array
+    const exercises = [...workout.exercises, 
+      {
+        exercise_id: workout.exercises.length + 1,
         name: exercise.name,
         type: exercise.type,
         sets: [],
@@ -19,36 +23,42 @@ const WorkoutForm = ({ changeRoute }) => {
         required: exercise.required,
         optional: exercise.optional
       }
-    ]);
+    ];
+    // Merge exercises array with workout and update state
+    updateWorkout({...workout, ...{ exercises: exercises }});
   }
 
   // Deletes an exercise
   const deleteExercise = (exerciseID) => {
-    const temp = [...exercises];
-    for (let i = exerciseID ; i < temp.length ; i++) {
-      temp[i-1] = temp[i];
-      temp[i-1].exercise_id--;
+    const exercises = [...workout.exercises];
+    // Shift each exercise above the deleted one down by 1 and update the IDs
+    for (let i = exerciseID ; i < exercises.length ; i++) {
+      exercises[i-1] = exercises[i];
+      exercises[i-1].exercise_id--;
     }
-    updateExercises(temp.slice(0, -1));
+    // Merge exercises array (ignoring last item) with workout and update state
+    updateWorkout({...workout, ...{ exercises: exercises.slice(0, -1) }});
   }
 
   // Deletes a set from a given exercise
   const deleteExerciseSet = (exerciseID, setID) => {
-    const temp = [...exercises];
-    const sets = exercises[exerciseID-1].sets;
+    const sets = workout.exercises[exerciseID-1].sets;
+    // Shift each set above the deleted one down by 1 and update the IDs
     for (let i = setID ; i < sets.length ; i++) {
       sets[i-1] = sets[i];
       sets[i-1].set_id--;
     }
-    temp[exerciseID-1].sets = sets.slice(0, -1);
-    updateExercises(temp);
+    const exercises = [...workout.exercises];
+    exercises[exerciseID-1].sets = sets.slice(0, -1);
+    updateWorkout({...workout, ...{ exercises: exercises }});
   }
 
   // Called by child component when the exercise has changed, updates the state
   const updateSingleExercise = (newExercise) => {
-    const exercisesCopy = [...exercises];
-    exercisesCopy[newExercise.exercise_id - 1] = newExercise;
-    updateExercises(exercisesCopy);
+    const exercises = [...workout.exercises];
+    exercises[newExercise.exercise_id - 1] = newExercise;
+    // Merge exercises array with workout and update state
+    updateWorkout({...workout, ...{ exercises: exercises }});
   }
 
   return (
@@ -56,11 +66,11 @@ const WorkoutForm = ({ changeRoute }) => {
       <div className='form'>
         <ExercisePicker addExercise={addExercise}/>
         <Workout 
-          exercises={exercises}
+          workout={workout}
           updateSingleExercise={updateSingleExercise}
           deleteExercise={deleteExercise}
           deleteExerciseSet={deleteExerciseSet}
-          changeRoute={changeRoute}
+          changeRoute={props.changeRoute}
         />
       </div>
     </>
